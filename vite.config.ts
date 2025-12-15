327df1fb3ed8cdbbf9f31d0981b20ab96e372db7
Michał Bastrzyk <michal.bastrzyk.pl@gmail.com>
2025-12-15 15:05:10 +0100

    feat: Added shadcn/ui and tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
+import path from'node:path'
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+
   plugins: [preact({
     reactAliasesEnabled: true,
-  })],
+  }), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
