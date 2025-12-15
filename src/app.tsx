84e51f01ba74f881260be1b00ac6a9b59c5731ec
Michał Bastrzyk <michal.bastrzyk.pl@gmail.com>
2025-12-15 15:11:29 +0100

    feat: Added biome for formatting

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,20 @@
-import { Button } from '@/components/ui/button'
-import { useState } from 'react'
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 export function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-    </>
-  )
+	return (
+		<>
+			<h1>Vite + Preact</h1>
+			<div class="card">
+				<Button onClick={() => setCount((count) => count + 1)}>
+					count is {count}
+				</Button>
+				<p>
+					Edit <code>src/app.tsx</code> and save to test HMR
+				</p>
+			</div>
+		</>
+	);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
-import path from'node:path'
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
-import tailwindcss from '@tailwindcss/vite'
+import path from "node:path";
+import preact from "@preact/preset-vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-
-  plugins: [preact({
-    reactAliasesEnabled: true,
-  }), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+	plugins: [
+		preact({
+			reactAliasesEnabled: true,
+		}),
+		tailwindcss(),
+	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+});
